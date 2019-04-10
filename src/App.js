3ea199432192d0198b421d8412d373894b1dbd49
WiFuchs <willfuchs13@gmail.com>
2019-04-10 10:59:46 -0700

    add Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Footer from "./components/layout/Footer.js";
+import Header from "./components/layout/Header.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import OtherPage from "./components/pages/OtherPage";
 
@@ -8,8 +9,10 @@ class App extends Component {
   render() {
     return (
       <Router>
-        This is constant
-        <Route path="/other" component={OtherPage} />
+        <Header />
+        <div className="content">
+          <Route path="/other" component={OtherPage} />
+        </div>
         <Footer />
       </Router>
     );
